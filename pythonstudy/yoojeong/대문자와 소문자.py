8c2a939e114b86dbdbbb7e9f1a3857af403d31df
Yoo Jeong Kim <105200175+jeongola@users.noreply.github.com>
2022-10-13 19:27:14 +0900

    Add files via upload

---
+++ pythonstudy/yoojeong/대문자와 소문자.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer=''
+    
+    for i in my_string:
+        if i.islower()==True:
+            answer+=i.upper()
+        elif i.islower()==False:
+            answer+=i.lower()
+
+    return answer
