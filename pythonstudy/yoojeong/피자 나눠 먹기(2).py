cbe109eef49dbf814b3700cfc28d88175f3b3795
Yoo Jeong Kim <105200175+jeongola@users.noreply.github.com>
2022-10-13 19:28:14 +0900

    Add files via upload

---
+++ pythonstudy/yoojeong/피자 나눠 먹기(2).py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    
+    
+    if n>7 & n%7!=0:
+        answer=n//7+1
+    elif n>7 & n%7==0:    
+        answer=n//7
+    elif n<=7:
+        answer=1
+    return answer
