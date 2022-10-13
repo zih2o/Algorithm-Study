58c01041bebbe524ebaba188e749c0e7e85b69d0
Yoo Jeong Kim <105200175+jeongola@users.noreply.github.com>
2022-10-13 19:35:13 +0900

    Add files via upload

---
+++ pythonstudy/yoojeong/짝수와 홀수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = [0,0]
+    
+    
+    for i in num_list:
+        if i%2==0:
+            answer[0]+=1
+        elif i%2!=0: 
+            answer[1]+=1
+    
+    return answer
